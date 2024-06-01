58334cdaae51c968bdf73706ee30d002406de953
Kateryna Voloshyna <123991493+VoloshynaK@users.noreply.github.com>
2024-06-01 10:55:13 -0400

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,13 +3,13 @@ import ReactDOM from 'react-dom/client'
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/es/integration/react'
 import App from './App.jsx'
-import {store} from './redux/store.js'
+import {store, persistor} from './redux/store.js'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-      <PersistGate loading={null} persistor={store.persistor}>
+      <PersistGate loading={null} persistor={persistor}>
         <App/>
       </PersistGate>
     </Provider>
